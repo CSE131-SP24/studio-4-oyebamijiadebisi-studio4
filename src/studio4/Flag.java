2a8c92d4f197c24c35ae7ce04e59afe860556a0d
13143 <13143@labake-lap.dhcp.wustl.edu>
2024-02-21 17:16:08 -0600

    flags and shapes

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,24 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color black = new Color (0, 0, 0);
+		StdDraw.filledSquare(0.5, 0.5, 1);
+		
+		Color cuteGreen = new Color (110, 237, 87);
+		StdDraw.setPenColor(cuteGreen);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.2);
+		
+		Color yt = new Color (255, 255, 255);
+		StdDraw.setPenColor(yt);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.075);
+		
+		Color yellow = new Color(255, 221, 90);
+		StdDraw.setPenColor(yellow);
+		
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		
+		StdDraw.setPenColor(black);
+		StdDraw.point(0.45, 0.5);
+		StdDraw.point(0.55, 0.5);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,19 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		Color pinky = new Color(255, 109, 182);
+		StdDraw.setPenColor(pinky);
+		StdDraw.filledRectangle(0.5, 0.5, .2, .2);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledSquare(0.5, 0.5, 0.2);
+		Color yellow = new Color(255, 221, 90);
+		StdDraw.setPenColor(yellow);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		
+		
+		
 		
 	}
 }
